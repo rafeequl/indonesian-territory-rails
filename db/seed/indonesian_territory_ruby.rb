7f77213ddf766d9581693ee2cddcb59a1586b293
Rafeequl Rahman <rafeequl@gmail.com>
2021-08-06 13:50:44 +0700

    disable districts and vilages

--- db/seed/indonesian_territory_ruby.rb
+++ db/seed/indonesian_territory_ruby.rb
@@ -1,4 +1,4 @@
 require File.expand_path('includes/ruby/provinces', __dir__)
 require File.expand_path('includes/ruby/regencies', __dir__)
-require File.expand_path('includes/ruby/districts', __dir__)
-require File.expand_path('includes/ruby/villages', __dir__)
+# require File.expand_path('includes/ruby/districts', __dir__)
+# require File.expand_path('includes/ruby/villages', __dir__)
